cbfade72ea388087ed8057cb3bef6b951ba87335
Michael Tiller <michael.tiller@gmail.com>
2021-05-12 21:21:03 -0400

    Curry decode

--- src/decode.spec.ts
+++ src/decode.spec.ts
@@ -2,11 +2,11 @@ import { decode } from "./decode";
 
 describe("Test decoding", () => {
   it("should retrieve just one string", () => {
-    const result = decode("foo", ["id"]);
+    const result = decode(["id"])("foo");
     expect(result).toEqual({ id: "foo" });
   });
   it("should extract two strings", () => {
-    const result = decode("foo:bar", ["id1", "id2"]);
+    const result = decode(["id1", "id2"])("foo:bar");
     expect(result).toEqual({ id1: "foo", id2: "bar" });
   });
 });
--- src/decode.ts
+++ src/decode.ts
@@ -1,33 +1,34 @@
 export function decode<T extends string>(
-  nss: string,
   x: [T]
-): Record<T, string>;
+): (nss: string) => Record<T, string>;
 export function decode<T1 extends string, T2 extends string>(
-  nss: string,
   x: [T1, T2]
-): Record<T1 | T2, string>;
+): (nss: string) => Record<T1 | T2, string>;
 export function decode<T1 extends string, T2 extends string, T3 extends string>(
-  nss: string,
   x: [T1, T2, T3]
-): Record<T1 | T2 | T3, string>;
+): (nss: string) => Record<T1 | T2 | T3, string>;
 export function decode<
   T1 extends string,
   T2 extends string,
   T3 extends string,
   T4 extends string
->(nss: string, x: [T1, T2, T3, T4]): Record<T1 | T2 | T3 | T4, string>;
+>(x: [T1, T2, T3, T4]): (nss: string) => Record<T1 | T2 | T3 | T4, string>;
 export function decode<
   T1 extends string,
   T2 extends string,
   T3 extends string,
   T4 extends string,
   T5 extends string
->(nss: string, x: [T1, T2, T3, T4, T5]): Record<T1 | T2 | T3 | T4 | T5, string>;
-export function decode(nss: string, x: string[]): Record<string, string> {
-  const ret: Record<string, string> = {};
-  const parts = nss.split(":");
-  if (parts.length !== x.length)
-    throw new Error(`Expected nss with ${x.length} segments but got ${nss}`);
-  x.forEach((key, i) => (ret[key] = parts[i]));
-  return ret;
+>(
+  x: [T1, T2, T3, T4, T5]
+): (nss: string) => Record<T1 | T2 | T3 | T4 | T5, string>;
+export function decode(x: string[]): (nss: string) => Record<string, string> {
+  return (nss: string) => {
+    const ret: Record<string, string> = {};
+    const parts = nss.split(":");
+    if (parts.length !== x.length)
+      throw new Error(`Expected nss with ${x.length} segments but got ${nss}`);
+    x.forEach((key, i) => (ret[key] = parts[i]));
+    return ret;
+  };
 }
